d79aa3d2dcbed195538f25fbcf00c6f414d35884
Alexandr Rudy <aoikomete@gmail.com>
2022-01-05 14:25:46 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,23 @@
 
 	'use strict';
 
-	var JetWooBuilderCustomIcon = {
+	let JetWooBuilderCustomIcon = {
 
 		init: function() {
 
-			let btnWrapper = $.find( '.single-product .jet-woo-btn-with-custom-icon-wrapper' );
+			let widgets = {
+				'jet-single-add-to-cart.default' : JetWooBuilderCustomIcon.widgetSingleAddToCart,
+			};
+
+			$.each( widgets, function( widget, callback ) {
+				window.elementorFrontend.hooks.addAction( 'frontend/element_ready/' + widget, callback );
+			} );
+
+		},
+
+		widgetSingleAddToCart: function( $scope ) {
+
+			let btnWrapper = $scope.find( '.jet-woo-btn-with-custom-icon-wrapper' );
 
 			if ( btnWrapper.length > 0 ) {
 				let content = $( btnWrapper ).data( 'content' );
@@ -14,23 +26,10 @@
 				$( btnWrapper ).find( '.single_add_to_cart_button' ).html( content );
 			}
 
-			elementor.channels.editor.on( 'section:activated', function ( sectionName, editor ) {
-				let editedElement = editor.getOption( 'editedElementView' ),
-					editorBtnWrapper = editedElement.$el.find( '.jet-woo-btn-with-custom-icon-wrapper' );
-
-				if ( editorBtnWrapper.length > 0 ) {
-					let content = $( editorBtnWrapper ).data( 'content' );
-
-					$( editorBtnWrapper ).find( '.single_add_to_cart_button' ).html( content );
-				}
-			} );
-
 		}
 
 	}
 
-	$( window ).on( 'elementor:init', JetWooBuilderCustomIcon.init );
-
-	window.JetWooBuilderCustomIcon = JetWooBuilderCustomIcon;
+	$( window ).on( 'elementor/frontend/init', JetWooBuilderCustomIcon.init );
 
 }( jQuery ) );
