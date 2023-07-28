97e7c558313257a6285b008bd6226d2cb6290c8a
Alexandr Rudy <aoikomete@gmail.com>
2023-07-28 16:31:40 +0300

    FIX: issue with default added to cart icon
    https://github.com/UraraReika/jet-woo-builder-custom-add-to-cart-icon/issues/2

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,30 @@
-// Load all necessary modules.
-let rename       = require('gulp-rename'),
-	sass         = require('gulp-sass'),
-	notify       = require('gulp-notify'),
-	autoprefixer = require('gulp-autoprefixer'),
-	sourcemaps   = require('gulp-sourcemaps');
+'use strict';
 
-const {src, dest, task, watch, series, parallel} = require('gulp');
+let rename = require( 'gulp-rename' ),
+	sass = require( 'gulp-sass' )( require( 'sass' ) ),
+	notify = require( 'gulp-notify' ),
+	autoprefixer = require( 'gulp-autoprefixer' );
 
-task('styles', function() {
-	return src('assets/scss/styles.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({
+const { src, dest, task, watch, series, parallel } = require( 'gulp' );
+
+task( 'styles', function () {
+	return src( 'assets/scss/styles.scss' )
+		.pipe( sass( {
 			errorLogToConsole: true,
-			outputStyle      : 'compressed'
-		}))
-		.on('error', console.error.bind(console))
-		.pipe(autoprefixer({
-			browsers: ['last 10 versions'],
-			cascade : false
-		}))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(sourcemaps.write('./'))
-		.pipe(dest('./assets/css/'))
-		.pipe(notify('Compile Admin Sass Done!'));
-});
+			outputStyle: 'compressed'
+		} ) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( autoprefixer( {
+			browsers: [ 'last 10 versions' ],
+			cascade: false
+		} ) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( dest( './assets/css/' ) )
+		.pipe( notify( 'Compile Sass Done!' ) );
+} );
 
-task('default', parallel('styles'));
+task( 'default', parallel( 'styles' ) );
 
-task('watch', function() {
-	watch('assets/scss/**/*.scss', series('styles'));
-});
+task( 'watch', function () {
+	watch( 'assets/scss/**/*.scss', series( 'styles' ) );
+} );
